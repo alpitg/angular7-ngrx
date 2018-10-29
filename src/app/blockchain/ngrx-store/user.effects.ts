85cf08f3a34a5d4a4bd061ec738d09aabe6abdb5
alpitg <alpitgajbhiye@gmail.com>
2018-10-29 11:00:57 -0700

    service with effect working code

--- src/app/blockchain/ngrx-store/user.effects.ts
+++ src/app/blockchain/ngrx-store/user.effects.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { map, debounceTime, distinctUntilChanged, switchMap } from '../../../../node_modules/rxjs/operators';
 import { AddCoins, ADD_COIN } from './blockchain.action';
+import { Observable } from '../../../../node_modules/rxjs';
+import { Action } from '../../../../node_modules/@ngrx/store';
 
 @Injectable()
 export class UserEffects {
@@ -10,26 +12,52 @@ export class UserEffects {
     constructor(
         private clientService: ClientService,
         private actions$: Actions
-    ) { console.log('EVENT----')}
+    ) { console.log('EVENT----') }
 
-    public fetch = (actions, role) => this.actions$.pipe(
-        ofType(actions),
-        map(action => actions),
-        debounceTime(400),
-        distinctUntilChanged(),
-        switchMap(partialAddress => this.clientService
-            .getRates()
-            .pipe(
-                map(results => new AddCoins(results)),
-                // map(results => results),
-            //   catchError(error => of(new FindAddressesRejected(error)))
-        )
-        )
-    );
+    // public fetch = (actions, role) => this.actions$.pipe(
+    //     ofType(actions),
+    //     map(action => actions),
+    //     debounceTime(400),
+    //     distinctUntilChanged(),
+    //     switchMap(partialAddress => this.clientService
+    //         .getRates()
+    //         .pipe(
+    //             map(results => new AddCoins(results)),
+    //         // map(results => results),
+    //         //   catchError(error => of(new FindAddressesRejected(error)))
+    //     )
+    //     )
+    // );
+
+
+
+    // @Effect()
+    // protected fetchEditors$ = this.fetch(ADD_COIN, "Editors")
+
+
 
 
 
     @Effect()
-    protected fetchEditors$ = this.fetch(ADD_COIN, "Editors")
+    loadAuths$: Observable<any> = this.actions$.pipe(
+        ofType(ADD_COIN), // whenever this action call this event triggered
+        switchMap(() => {
+            return this.clientService.getData()
+        })
+    );
 
+
+
+    // @Effect()
+    // loadAuths$: Observable<Action> = this.actions$.pipe(
+    //   ofType(authActions.AuthActionTypes.LoadAuths),
+    //   switchMap(() => {
+    //     return this.http.get<string>('login')
+    //       .pipe(
+    //         map((userName) => {
+    //           return new authActions.SetAuths(userName);
+    //         })
+    //       )
+    //   })
+    // );
 }
